4246a7cb730c61e96bbf6dbb96c383995ee5e8c8
Michael Jacobsen <mj@iqinabox.com>
2022-11-09 09:18:30 +0100

    Disable projective, add which-key

--- startup.el
+++ startup.el
@@ -83,17 +83,22 @@
   (setq use-package-always-ensure t
 	use-package-expand-minimally t))
 
-(use-package projectile
-	     :ensure t
-	     :init
-	     (projectile-mode +1)
-	     :config
-	     (setq projectile-project-search-path '("c:/Work/IQinABox"))
-	     :bind (:map projectile-mode-map
-			 ("s-p" . projectile-command-map)
-			 ("C-c p" . projectile-command-map)))
+;; (use-package projectile
+;; 	     :ensure t
+;; 	     :init
+;; 	     (projectile-mode +1)
+;; 	     :config
+;; 	     (setq projectile-project-search-path '("c:/Work/IQinABox"))
+;; 	     :bind (:map projectile-mode-map
+;; 			 ("s-p" . projectile-command-map)
+;; 			 ("C-c p" . projectile-command-map)))
 ;; Ivy and friends setup from
 ;; https://www.reddit.com/r/emacs/comments/910pga/tip_how_to_use_ivy_and_its_utilities_in_your/
+(use-package which-key
+  :ensure t
+  :config (which-key-mode)
+  )
+
 (use-package ivy
   :ensure t
   :custom
